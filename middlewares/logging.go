8c1948cc480907511f15785d658c6f447f6c9ad7
Zhang He <zhang_he06@163.com>
2018-12-28 20:55:10 +0800

    更新日志中间件

--- middlewares/logging.go
+++ middlewares/logging.go
@@ -1,11 +1,15 @@
 package middlewares
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/zhanghe06/gin_project/logs"
+	"io"
+	"io/ioutil"
 	"time"
 )
 
@@ -13,6 +17,29 @@ func LoggingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 开始时间
 		start := time.Now()
+
+		// 获取请求
+		bodyCopy := new(bytes.Buffer)
+		// Read the whole body
+		_, err := io.Copy(bodyCopy, c.Request.Body)
+		if err != nil {
+			return  // 退出中间件
+		}
+		bodyData := bodyCopy.Bytes()
+		// Replace the body with a reader that reads from the buffer
+		c.Request.Body = ioutil.NopCloser(bytes.NewReader(bodyData))
+
+		var requestParams string
+		requestParams = string(bodyData) // 默认原始
+
+		decoder := json.NewDecoder(bodyCopy)
+		var paramsMap map[string]interface{}
+		if err = decoder.Decode(&paramsMap); err == nil {
+			paramsByte, e := json.Marshal(paramsMap)
+			if e == nil {
+				requestParams = string(paramsByte) // 反向解析
+			}
+		}
 
 		c.Next()
 
@@ -24,11 +51,14 @@ func LoggingMiddleware() gin.HandlerFunc {
 		method := c.Request.Method
 		statusCode := c.Writer.Status()
 		logFields := log.Fields{
-			"project":     viper.GetString("project.name"),
-			"latency":     fmt.Sprintf("%v", latency),
-			"client_ip":   clientIP,
-			"method":      method,
-			"status_code": statusCode,
+			"project":        viper.GetString("project.name"),
+			"latency":        fmt.Sprintf("%v", latency),
+			"client_ip":      clientIP,
+			"host":           c.Request.Host,
+			"method":         method,
+			"request_uri":    c.Request.RequestURI,
+			"request_params": requestParams,
+			"status_code":    statusCode,
 		}
 		logs.Log = logs.Logger.WithFields(logFields)
 
